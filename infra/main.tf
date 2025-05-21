3e1ff4ebdcc346aae5d41fb5d444137b19f43e4e
jcontreras549 <jef.contreras.549@gmail.com>
2025-05-21 18:55:23 -0500

    infra

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,25 @@ module "ecr_repo" {
   image_mutability = "IMMUTABLE"
 }
 
+resource "aws_security_group" "default" {
+  name        = "ec2-default-sg"
+  description = "Default security group for EC2"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 module "ec2" {
   source             = "../modules/ec2"
@@ -20,7 +39,7 @@ module "ec2" {
   instance_type      = "t3.micro"
   subnet_id          = var.public_subnet_id
   key_name           = var.key_name
-  security_group_ids = []
+  security_group_ids = [aws_security_group.default.id]
   tags = {
     Name        = var.ec2_name
     Environment = "demo"
