79c9327d0756ddf38e46544832ec67f0ac0ac4b3
jcontreras549 <jef.contreras.549@gmail.com>
2025-05-21 18:22:18 -0500

    infra

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "bucket-terraform-utec"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "ecr_repo" {
   source           = "../modules/ecr"
   ecr_name         = var.ecr_names
