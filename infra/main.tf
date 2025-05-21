2349095664a3f66fc9bf9d3241ad25df5af96074
jcontreras549 <jef.contreras.549@gmail.com>
2025-05-21 16:54:19 -0500

    estructure

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,12 @@
-# module "ecr_repo" {
-#   source           = "./modules/ecr"
-#   ecr_name         = var.ecr_names
-#   tags             = var.tags
-#   image_mutability = "IMMUTABLE"
-# }
+module "ecr_repo" {
+  source           = "../modules/ecr"
+  ecr_name         = var.ecr_names
+  tags             = var.tags
+  image_mutability = "IMMUTABLE"
+}
 
 # module "ec2" {
-#   source             = "./modules/ec2"
+#   source             = "../modules/ec2"
 #   ami                = "ami-0c02fb55956c7d316"
 #   instance_type      = "t3.micro"
 #   subnet_id          = var.public_subnet_id
@@ -19,7 +19,7 @@
 # }
 
 # module "eks" {
-#   source = "./modules/eks"
+#   source = "../modules/eks"
 
 #   cluster_name    = var.cluster_name
 #   cluster_version = var.cluster_version
